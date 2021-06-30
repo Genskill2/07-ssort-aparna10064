e3340b47f90cde1340f437776fc8b7c7ee5a7841
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-06-30 22:13:35 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+void ssort(int arr[], int l){
+  for(int i=0; i<l; i++) {
+    swap_max(arr,l,i);
+  }
+}
+void swap_max(int arr[], int l, int n)
+{
+  int i,j;
+  int temp;
+  for(j=i=n;j<l;j++)
+    {
+        if(arr[j] > arr[i])
+        {
+            i = j;
+        }
+    }
+
+    temp = arr[n];
+    arr[n] = arr[i];
+    arr[i] = temp;
+}
